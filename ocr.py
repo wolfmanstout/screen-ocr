3906aefab497199bc959da81826d792972a80843
James Stout <james.wolf.stout@gmail.com>
2019-12-31 12:43:21 -0800

    Update grid search.

--- ocr.py
+++ ocr.py
@@ -104,25 +104,31 @@ def preprocess(image,
 class OcrEstimator(BaseEstimator):
     def __init__(self,
                  threshold_type=None,
-                 threshold_block_size=None,
-                 correction_block_size=None,
+                 block_size=None,
                  margin=None,
-                 resize_factor=None):
+                 resize_factor=None,
+                 convert_grayscale=None,
+                 shift_channels=None,
+                 label_components=None):
         self.threshold_type = threshold_type
-        self.threshold_block_size = threshold_block_size
-        self.correction_block_size = correction_block_size
+        self.block_size = block_size
         self.margin = margin
         self.resize_factor = resize_factor
+        self.convert_grayscale = convert_grayscale
+        self.shift_channels = shift_channels
+        self.label_components = label_components
 
     def fit(self, X=None, y=None):
         if self.threshold_type == "otsu":
             self.threshold_function_ = lambda data: filters.threshold_otsu(data)
+        elif self.threshold_type == "local_otsu":
+            self.threshold_function_ = lambda data: filters.rank.otsu(data, morphology.square(self.block_size))
         elif self.threshold_type == "local":
-            self.threshold_function_ = lambda data: filters.threshold_local(data, self.threshold_block_size)
+            self.threshold_function_ = lambda data: filters.threshold_local(data, self.block_size)
         elif self.threshold_type == "niblack":
-            self.threshold_function_ = lambda data: filters.threshold_niblack(data, self.threshold_block_size)
+            self.threshold_function_ = lambda data: filters.threshold_niblack(data, self.block_size)
         elif self.threshold_type == "sauvola":
-            self.threshold_function_ = lambda data: filters.threshold_sauvola(data, self.threshold_block_size)
+            self.threshold_function_ = lambda data: filters.threshold_sauvola(data, self.block_size)
         else:
             raise ValueError("Unknown threshold type: {}".format(self.threshold_type))
 
@@ -131,9 +137,12 @@ class OcrEstimator(BaseEstimator):
         for image, gt_text in zip(X, y):
             image = preprocess(image,
                                threshold_function=self.threshold_function_,
-                               correction_block_size=self.correction_block_size,
+                               correction_block_size=self.block_size,
                                margin=self.margin,
-                               resize_factor=self.resize_factor)
+                               resize_factor=self.resize_factor,
+                               convert_grayscale=self.convert_grayscale,
+                               shift_channels=self.shift_channels,
+                               label_components=self.label_components)
             # Assume "api" is set globally. This is easier than making it a
             # param because it does not support deepcopy.
             result = native_image_to_string(api, image)
@@ -142,28 +151,28 @@ class OcrEstimator(BaseEstimator):
             
 
 os.chdir(r"C:\Users\james\Documents\OCR")
-for debug_image in glob.glob("debug*.png"):
-  os.remove(debug_image)
+for debug_output in glob.glob("debug*"):
+  os.remove(debug_output)
 
 # Load image and crop.
 bounding_box = (0, 0, 200, 200)
-image_path = "pillow_docs.png"
-image = Image.open(image_path).convert("RGB").crop(bounding_box)
+image_path = "train/pillow_docs.png"
+image = Image.open(image_path).convert("RGB") #.crop(bounding_box)
 # image = ImageGrab.grab(bounding_box)
 
 # Preprocess the image.
 block_size = 51
-threshold_function = lambda data: filters.threshold_local(data, block_size)
+threshold_function = lambda data: filters.rank.otsu(data, morphology.square(block_size))
 margin = 40
-resize_factor = 3
+resize_factor = 4
 convert_grayscale = True
 shift_channels = True
-label_components = True
+label_components = False
 preprocessing_time = timeit.timeit("global preprocessed_image; preprocessed_image = preprocess(image, threshold_function, block_size, margin, resize_factor, convert_grayscale, shift_channels, label_components)", globals=globals(), number=1)
 preprocessed_image.save("debug.png")
 
 # Load ground truth.
-gt_path = "pillow_docs_cropped_gt.txt"
+gt_path = "train/pillow_docs.txt"
 with open(gt_path, "r") as gt_file:
     gt_string = gt_file.read()
 
@@ -173,34 +182,40 @@ data_path = r"C:\Program Files\Tesseract-OCR\tessdata"
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 tessdata_dir_config = r'--tessdata-dir "{}"'.format(data_path)
 with PyTessBaseAPI(path=data_path) as api:
-    native_string = None
-    native_time = timeit.timeit("global native_string; native_string = native_image_to_string(api, preprocessed_image)", globals=globals(), number=1)
-    native_cost = cost(native_string, gt_string)
-    binary_string = None
-    binary_time = timeit.timeit("global binary_string; binary_string = pytesseract.image_to_string(preprocessed_image, config=tessdata_dir_config)", globals=globals(), number=1)
-    binary_cost = cost(binary_string, gt_string)
-    print(native_string)
-    print("------------------")
-    print(binary_string)
-    print("preprocessing time: {:f}".format(preprocessing_time))
-    print("native\ttime: {:.2f}\tcost: {:.2f}".format(native_time, native_cost))
-    print("binary\ttime: {:.2f}\tcost: {:.2f}".format(binary_time, binary_cost))
+    # native_string = None
+    # native_time = timeit.timeit("global native_string; native_string = native_image_to_string(api, preprocessed_image)", globals=globals(), number=1)
+    # native_cost = cost(native_string, gt_string)
+    # binary_string = None
+    # binary_time = timeit.timeit("global binary_string; binary_string = pytesseract.image_to_string(preprocessed_image, config=tessdata_dir_config)", globals=globals(), number=1)
+    # binary_cost = cost(binary_string, gt_string)
+    # with open("debug_native.txt", "w") as file:
+    #     file.write(native_string)
+    # with open("debug_binary.txt", "w") as file:
+    #     file.write(binary_string)
+    # print(native_string)
+    # print("------------------")
+    # print(binary_string)
+    # print("preprocessing time: {:f}".format(preprocessing_time))
+    # print("native\ttime: {:.2f}\tcost: {:.2f}".format(native_time, native_cost))
+    # print("binary\ttime: {:.2f}\tcost: {:.2f}".format(binary_time, binary_cost))
 
-    # X = [image]
-    # y = [gt_string]
-    # grid_search = model_selection.GridSearchCV(
-    #     OcrEstimator(),
-    #     {
-    #         "threshold_type": ["local", "niblack", "sauvola"],
-    #         "threshold_block_size": [41, 51, 61],
-    #         "correction_block_size": [41, 51, 61],
-    #         "margin": [40],
-    #         "resize_factor": [4],
-    #     },
-    #     cv=model_selection.PredefinedSplit([0] * len(y))
-    # )
-    # grid_search.fit(X, y)
-    # results = pd.DataFrame(grid_search.cv_results_)
-    # results.set_index("params", inplace=True)
-    # print(results["mean_test_score"].sort_values(ascending=False))
-    # print(grid_search.best_params_)
+    X = [image]
+    y = [gt_string]
+    grid_search = model_selection.GridSearchCV(
+        OcrEstimator(),
+        {
+            "threshold_type": ["otsu", "local_otsu", "local", "niblack", "sauvola"],
+            "block_size": [51, 61, 71],
+            "margin": [40],
+            "resize_factor": [3, 4],
+            "convert_grayscale": [True],
+            "shift_channels": [False, True],
+            "label_components": [False],
+        },
+        cv=model_selection.PredefinedSplit([0] * len(y))
+    )
+    grid_search.fit(X, y)
+    results = pd.DataFrame(grid_search.cv_results_)
+    results.set_index("params", inplace=True)
+    print(results["mean_test_score"].sort_values(ascending=False))
+    print(grid_search.best_params_)
