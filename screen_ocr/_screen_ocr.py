0eb11413795c683f1757d4e4f745d6f09a077ddb
James Stout <james.wolf.stout@gmail.com>
2020-08-09 11:59:02 -0700

    Support preprocessing across all backends.

--- screen_ocr/_screen_ocr.py
+++ screen_ocr/_screen_ocr.py
@@ -1,9 +1,12 @@
 """Library for processing screen contents using OCR."""
 
+import numpy as np
+import pandas as pd
+from PIL import Image, ImageDraw, ImageGrab, ImageOps
 from fuzzywuzzy import fuzz
-from PIL import ImageGrab
-from skimage import filters, morphology
+from skimage import filters, morphology, transform
 
+from . import _base
 from . import _tesseract
 
 # Optional packages.
@@ -24,101 +27,102 @@ class Reader(object):
 
     @classmethod
     def create_quality_reader(cls,
-                              confidence_threshold=None,
-                              radius=None,
+                              tesseract_data_path=None,
+                              tesseract_command=None,
                               **kwargs):
         """Create reader optimized for quality.
 
-        See TesseractReader constructor for full argument list.
+        See constructor for full argument list.
         """
         backend = _tesseract.TesseractBackend(
-            threshold_function=lambda data: filters.rank.otsu(data, morphology.square(41)),
-            correction_block_size=31,
-            margin=50,
-            resize_factor=2,
-            convert_grayscale=True,
-            shift_channels=True,
-            label_components=False,
-            **kwargs)
+            tesseract_data_path=tesseract_data_path,
+            tesseract_command=tesseract_command)
         return cls(backend,
-                   confidence_threshold=confidence_threshold,
-                   radius=radius)
+                   threshold_function=lambda data: filters.rank.otsu(data, morphology.square(41)),
+                   correction_block_size=31,
+                   margin=50,
+                   resize_factor=2,
+                   convert_grayscale=True,
+                   shift_channels=True,
+                   label_components=False,
+                   **kwargs)
 
     @classmethod
     def create_fast_reader(cls,
-                           confidence_threshold=None,
-                           radius=None,
+                           tesseract_data_path=None,
+                           tesseract_command=None,
                            **kwargs):
         """Create reader optimized for speed.
 
-        See TesseractReader constructor for full argument list.
+        See constructor for full argument list.
         """
         backend = _tesseract.TesseractBackend(
-            threshold_function=lambda data: filters.threshold_otsu(data),
-            correction_block_size=41,
-            margin=60,
-            resize_factor=2,
-            convert_grayscale=True,
-            shift_channels=True,
-            label_components=False,
-            **kwargs)
+            tesseract_data_path=tesseract_data_path,
+            tesseract_command=tesseract_command)
         return cls(backend,
-                   confidence_threshold=confidence_threshold,
-                   radius=radius)
+                   threshold_function=lambda data: filters.threshold_otsu(data),
+                   correction_block_size=41,
+                   margin=60,
+                   resize_factor=2,
+                   convert_grayscale=True,
+                   shift_channels=True,
+                   label_components=False,
+                   **kwargs)
 
     @classmethod
     def create_reader(cls,
                       backend,
-                      confidence_threshold=None,
-                      radius=None,
-                      debug_image_callback=None,
                       **kwargs):
         """Create reader with specified backend."""
         if backend == "tesseract":
-            return cls.create_quality_reader(
-                confidence_threshold=confidence_threshold,
-                radius=radius,
-                debug_image_callback=debug_image_callback,
-                **kwargs)
+            return cls.create_quality_reader(**kwargs)
         elif backend == "easyocr":
-            backend = _easyocr.EasyOcrBackend(**kwargs)
-            return cls(backend,
-                       confidence_threshold=confidence_threshold,
-                       radius=radius)
+            backend = _easyocr.EasyOcrBackend()
+            return cls(backend, **kwargs)
         elif backend == "winrt":
-            backend = _winrt.WinRtBackend(**kwargs)
-            return cls(backend,
-                       confidence_threshold=confidence_threshold,
-                       radius=radius)
+            backend = _winrt.WinRtBackend()
+            return cls(backend, **kwargs)
         else:
-            return cls(backend,
-                       confidence_threshold=confidence_threshold,
-                       radius=radius)
+            return cls(backend, **kwargs)
 
     def __init__(self,
                  backend,
+                 threshold_function=None,
+                 correction_block_size=None,
+                 margin=0,
+                 resize_factor=1,
+                 convert_grayscale=False,
+                 shift_channels=False,
+                 label_components=False,
+                 debug_image_callback=None,
                  confidence_threshold=None,
                  radius=None):
         self._backend = backend
+        self.threshold_function = threshold_function
+        self.correction_block_size = correction_block_size
+        self.margin = margin
+        self.resize_factor = resize_factor
+        self.convert_grayscale = convert_grayscale
+        self.shift_channels = shift_channels
+        self.label_components = label_components
+        self.debug_image_callback = debug_image_callback
         self.confidence_threshold = confidence_threshold or 0.75
         self.radius = radius or 100
 
     def read_nearby(self, screen_coordinates):
         """Return ScreenContents nearby the provided coordinates."""
         screenshot, bounding_box = self._screenshot_nearby(screen_coordinates)
-        result = self._backend.run_ocr(screenshot)
-        return ScreenContents(screen_coordinates=screen_coordinates,
-                              screenshot=screenshot,
-                              offset=bounding_box[0:2],
-                              result=result,
-                              confidence_threshold=self.confidence_threshold)
+        return self.read_image(screenshot,
+                               offset=bounding_box[0:2],
+                               screen_coordinates=screen_coordinates)
 
-    def read_image(self, image):
+    def read_image(self, image, offset=(0, 0), screen_coordinates=(0, 0)):
         """Return ScreenContents of the provided image."""
-        result = self._backend.run_ocr(image)
-        return ScreenContents(screen_coordinates=(0, 0),
+        preprocessed_image = self._preprocess(image)
+        result = self._backend.run_ocr(preprocessed_image)
+        result = self._adjust_result(result, offset)
+        return ScreenContents(screen_coordinates=screen_coordinates,
                               screenshot=image,
-                              offset=(0, 0),
                               result=result,
                               confidence_threshold=self.confidence_threshold)
 
@@ -134,6 +138,121 @@ class Reader(object):
         screenshot = screenshot.crop(bounding_box)
         return screenshot, bounding_box
 
+    def _adjust_result(self, result, offset):
+        lines = []
+        for line in result.lines:
+            words = []
+            for word in line.words:
+                left = ((word.left - self.margin) / self.resize_factor) + offset[0]
+                top = ((word.top - self.margin) / self.resize_factor) + offset[1]
+                width = word.width / self.resize_factor
+                height = word.height / self.resize_factor
+                words.append(_base.OcrWord(word.text, left, top, width, height))
+            lines.append(_base.OcrLine(words))
+        return _base.OcrResult(lines)
+
+    def _preprocess(self, image):
+        if self.resize_factor != 1:
+            new_size = (image.size[0] * self.resize_factor, image.size[1] * self.resize_factor)
+            image = image.resize(new_size, Image.NEAREST)
+        if self.debug_image_callback:
+            self.debug_image_callback("debug_resized", image)
+
+        data = np.array(image)
+        if self.shift_channels:
+            channels = [self._shift_channel(data[:, :, i], i) for i in range(3)]
+            data = np.stack(channels, axis=-1)
+
+        if self.threshold_function:
+            if self.convert_grayscale:
+                image = Image.fromarray(data)
+                image = image.convert("L")
+                data = np.array(image)
+                data = self._binarize_channel(data, None)
+            else:
+                channels = [self._binarize_channel(data[:, :, i], i)
+                            for i in range(3)]
+                data = np.stack(channels, axis=-1)
+                data = np.all(data, axis=-1)
+
+        image = Image.fromarray(data)
+        if self.margin:
+            image = ImageOps.expand(image, self.margin, "white")
+        # Ensure consistent performance measurements.
+        image.load()
+        if self.debug_image_callback:
+            self.debug_image_callback("debug_final", image)
+        return image
+
+    def _binarize_channel(self, data, channel_index):
+        if self.debug_image_callback:
+            self.debug_image_callback("debug_before_{}".format(channel_index), Image.fromarray(data))
+        # Necessary to avoid ValueError from Otsu threshold.
+        if data.min() == data.max():
+            threshold = np.uint8(0)
+        else:
+            threshold = self.threshold_function(data)
+        if self.debug_image_callback:
+            if threshold.ndim == 2:
+                self.debug_image_callback("debug_threshold_{}".format(channel_index), Image.fromarray(threshold.astype(np.uint8)))
+            else:
+                self.debug_image_callback("debug_threshold_{}".format(channel_index), Image.fromarray(np.ones_like(data) * threshold))
+        data = data > threshold
+        if self.label_components:
+            labels, num_labels = measure.label(data, background=-1, return_num=True)
+            label_colors = np.zeros(num_labels + 1, np.bool_)
+            label_colors[labels] = data
+            background_labels = filters.rank.modal(labels.astype(np.uint16, copy=False),
+                                                   morphology.square(self.correction_block_size))
+            background_colors = label_colors[background_labels]
+        else:
+            white_sums = self._window_sums(data, self.correction_block_size)
+            black_sums = self._window_sums(~data, self.correction_block_size)
+            background_colors = white_sums > black_sums
+            # background_colors = filters.rank.modal(data.astype(np.uint8, copy=False),
+            #                                        morphology.square(self.correction_block_size))
+        if self.debug_image_callback:
+            self.debug_image_callback("debug_background_{}".format(channel_index), Image.fromarray(background_colors == True))
+        # Make the background consistently white (True).
+        data = data == background_colors
+        if self.debug_image_callback:
+            self.debug_image_callback("debug_after_{}".format(channel_index), Image.fromarray(data))
+        return data
+
+    @staticmethod
+    def _window_sums(image, window_size):
+        integral = transform.integral_image(image)
+        radius = int((window_size - 1) / 2)
+        top_left = np.zeros(image.shape, dtype=np.uint16)
+        top_left[radius:, radius:] = integral[:-radius, :-radius]
+        top_right = np.zeros(image.shape, dtype=np.uint16)
+        top_right[radius:, :-radius] = integral[:-radius, radius:]
+        top_right[radius:, -radius:] = integral[:-radius, -1:]
+        bottom_left = np.zeros(image.shape, dtype=np.uint16)
+        bottom_left[:-radius, radius:] = integral[radius:, :-radius]
+        bottom_left[-radius:, radius:] = integral[-1:, :-radius]
+        bottom_right = np.zeros(image.shape, dtype=np.uint16)
+        bottom_right[:-radius, :-radius] = integral[radius:, radius:]
+        bottom_right[-radius:, :-radius] = integral[-1:, radius:]
+        bottom_right[:-radius, -radius:] = integral[radius:, -1:]
+        bottom_right[-radius:, -radius:] = integral[-1, -1]
+        return bottom_right - bottom_left - top_right + top_left
+
+    @staticmethod
+    def _shift_channel(data, channel_index):
+        """Shifts each channel based on actual position in a typical LCD. This reduces
+        artifacts from subpixel rendering. Note that this assumes RGB left-to-right
+        ordering and a subpixel size of 1 in the resized image.
+        """
+        channel_shift = channel_index - 1
+        if channel_shift != 0:
+            data = np.roll(data, channel_shift, axis=1)
+            if channel_shift == -1:
+                data[:, -1] = data[:, -2]
+            elif channel_shift == 1:
+                data[:, 0] = data[:, 1]
+        return data
+
 
 class ScreenContents(object):
     """OCR'd contents of a portion of the screen."""
@@ -141,12 +260,10 @@ class ScreenContents(object):
     def __init__(self,
                  screen_coordinates,
                  screenshot,
-                 offset,
                  result,
                  confidence_threshold):
         self.screen_coordinates = screen_coordinates
         self.screenshot = screenshot
-        self.offset = offset
         self.result = result
         self.confidence_threshold = confidence_threshold
 
@@ -186,8 +303,8 @@ class ScreenContents(object):
                             if score == max([score for score, _ in scored_words])]
 
         # Next, find the closest match based on screen distance.
-        distance_to_words = [(self.distance_squared(word.center[0] + self.offset[0],
-                                                    word.center[1] + self.offset[1],
+        distance_to_words = [(self.distance_squared(word.center[0],
+                                                    word.center[1],
                                                     *self.screen_coordinates), word)
                              for word in possible_matches]
         best_match = min(distance_to_words, key=lambda x: x[0])[1]
@@ -197,7 +314,7 @@ class ScreenContents(object):
             x = best_match.center[0]
         elif cursor_position == "after":
             x = best_match.left + best_match.width
-        return (int(x + self.offset[0]), int(best_match.center[1] + self.offset[1]))
+        return (int(x), int(best_match.center[1]))
 
     def _score_word(self, candidate, normalized_target):
         candidate = candidate.lower().replace(u'\u2019', '\'')
--- screen_ocr/_tesseract.py
+++ screen_ocr/_tesseract.py
@@ -1,45 +1,22 @@
-import numpy as np
-import pandas as pd
 import pytesseract
 import six
-from PIL import Image, ImageDraw, ImageGrab, ImageOps
-from skimage import filters, morphology, transform
 
 from . import _base
 
 
 class TesseractBackend(_base.OcrBackend):
     def __init__(self,
-                 threshold_function,
-                 correction_block_size,
-                 margin,
-                 resize_factor,
-                 convert_grayscale,
-                 shift_channels,
-                 label_components,
-                 debug_image_callback=None,
-                 tesseract_data_path=r"C:\Program Files\Tesseract-OCR\tessdata",
-                 tesseract_command=r"C:\Program Files\Tesseract-OCR\tesseract.exe"):
-        self.threshold_function = threshold_function
-        self.correction_block_size = correction_block_size
-        self.margin = margin
-        self.resize_factor = resize_factor
-        self.convert_grayscale = convert_grayscale
-        self.shift_channels = shift_channels
-        self.label_components = label_components
-        self.debug_image_callback = debug_image_callback
-        self.tesseract_data_path = tesseract_data_path
-        self.tesseract_command = tesseract_command
+                 tesseract_data_path=None,
+                 tesseract_command=None):
+        self.tesseract_data_path = tesseract_data_path or r"C:\Program Files\Tesseract-OCR\tessdata"
+        self.tesseract_command = tesseract_command or r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
     def run_ocr(self, image):
-        preprocessed_image = self._preprocess(image)
         tessdata_dir_config = r'--tessdata-dir "{}"'.format(self.tesseract_data_path)
         pytesseract.pytesseract.tesseract_cmd = self.tesseract_command
-        results = pytesseract.image_to_data(preprocessed_image,
+        results = pytesseract.image_to_data(image,
                                             config=tessdata_dir_config,
                                             output_type=pytesseract.Output.DATAFRAME)
-        results[["top", "left"]] = (results[["top", "left"]] - self.margin) / self.resize_factor
-        results[["width", "height"]] = results[["width", "height"]] / self.resize_factor
         lines = []
         words = []
         for _, box in results.iterrows():
@@ -60,102 +37,3 @@ class TesseractBackend(_base.OcrBackend):
         lines.sort(key=lambda line: (line.words[0].top, line.words[0].left))
         return _base.OcrResult(lines)
 
-    def _preprocess(self, image):
-        new_size = (image.size[0] * self.resize_factor, image.size[1] * self.resize_factor)
-        image = image.resize(new_size, Image.NEAREST)
-        if self.debug_image_callback:
-            self.debug_image_callback("debug_resized", image)
-
-        data = np.array(image)
-        if self.shift_channels:
-            channels = [self._shift_channel(data[:, :, i], i) for i in range(3)]
-            data = np.stack(channels, axis=-1)
-
-        if self.convert_grayscale:
-            image = Image.fromarray(data)
-            image = image.convert("L")
-            data = np.array(image)
-            data = self._binarize_channel(data, None)
-            image = Image.fromarray(data)
-        else:
-            channels = [self._binarize_channel(data[:, :, i], i)
-                        for i in range(3)]
-            data = np.stack(channels, axis=-1)
-            data = np.all(data, axis=-1)
-            image = Image.fromarray(data)
-
-        image = ImageOps.expand(image, self.margin, "white")
-        # Ensure consistent performance measurements.
-        image.load()
-        if self.debug_image_callback:
-            self.debug_image_callback("debug_final", image)
-        return image
-
-    def _binarize_channel(self, data, channel_index):
-        if self.debug_image_callback:
-            self.debug_image_callback("debug_before_{}".format(channel_index), Image.fromarray(data))
-        # Necessary to avoid ValueError from Otsu threshold.
-        if data.min() == data.max():
-            threshold = np.uint8(0)
-        else:
-            threshold = self.threshold_function(data)
-        if self.debug_image_callback:
-            if threshold.ndim == 2:
-                self.debug_image_callback("debug_threshold_{}".format(channel_index), Image.fromarray(threshold.astype(np.uint8)))
-            else:
-                self.debug_image_callback("debug_threshold_{}".format(channel_index), Image.fromarray(np.ones_like(data) * threshold))
-        data = data > threshold
-        if self.label_components:
-            labels, num_labels = measure.label(data, background=-1, return_num=True)
-            label_colors = np.zeros(num_labels + 1, np.bool_)
-            label_colors[labels] = data
-            background_labels = filters.rank.modal(labels.astype(np.uint16, copy=False),
-                                                   morphology.square(self.correction_block_size))
-            background_colors = label_colors[background_labels]
-        else:
-            white_sums = self._window_sums(data, self.correction_block_size)
-            black_sums = self._window_sums(~data, self.correction_block_size)
-            background_colors = white_sums > black_sums
-            # background_colors = filters.rank.modal(data.astype(np.uint8, copy=False),
-            #                                        morphology.square(self.correction_block_size))
-        if self.debug_image_callback:
-            self.debug_image_callback("debug_background_{}".format(channel_index), Image.fromarray(background_colors == True))
-        # Make the background consistently white (True).
-        data = data == background_colors
-        if self.debug_image_callback:
-            self.debug_image_callback("debug_after_{}".format(channel_index), Image.fromarray(data))
-        return data
-
-    @staticmethod
-    def _window_sums(image, window_size):
-        integral = transform.integral_image(image)
-        radius = int((window_size - 1) / 2)
-        top_left = np.zeros(image.shape, dtype=np.uint16)
-        top_left[radius:, radius:] = integral[:-radius, :-radius]
-        top_right = np.zeros(image.shape, dtype=np.uint16)
-        top_right[radius:, :-radius] = integral[:-radius, radius:]
-        top_right[radius:, -radius:] = integral[:-radius, -1:]
-        bottom_left = np.zeros(image.shape, dtype=np.uint16)
-        bottom_left[:-radius, radius:] = integral[radius:, :-radius]
-        bottom_left[-radius:, radius:] = integral[-1:, :-radius]
-        bottom_right = np.zeros(image.shape, dtype=np.uint16)
-        bottom_right[:-radius, :-radius] = integral[radius:, radius:]
-        bottom_right[-radius:, :-radius] = integral[-1:, radius:]
-        bottom_right[:-radius, -radius:] = integral[radius:, -1:]
-        bottom_right[-radius:, -radius:] = integral[-1, -1]
-        return bottom_right - bottom_left - top_right + top_left
-
-    @staticmethod
-    def _shift_channel(data, channel_index):
-        """Shifts each channel based on actual position in a typical LCD. This reduces
-        artifacts from subpixel rendering. Note that this assumes RGB left-to-right
-        ordering and a subpixel size of 1 in the resized image.
-        """
-        channel_shift = channel_index - 1
-        if channel_shift != 0:
-            data = np.roll(data, channel_shift, axis=1)
-            if channel_shift == -1:
-                data[:, -1] = data[:, -2]
-            elif channel_shift == 1:
-                data[:, 0] = data[:, 1]
-        return data
