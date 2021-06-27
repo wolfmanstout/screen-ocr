a4205294c22a09fae52dfc39e538edd73874b801
James Stout <james.wolf.stout@gmail.com>
2021-06-27 15:25:42 -0700

    Updated version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="screen-ocr",
-    version="0.1.1",
+    version="0.2.0",
     author="James Stout",
     author_email="james.wolf.stout@gmail.com",
     description="Library for processing screen contents using OCR",
